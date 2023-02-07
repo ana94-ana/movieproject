ffea11e8b6094465ee9678d6e1081f2a06bf919b
ana94-ana <aniabramishvili94@gmail.com>
2023-02-07 22:18:14 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>

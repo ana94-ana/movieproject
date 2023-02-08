1e12a27133c88252f835be5518f85ac039ab24ce
ana94-ana <aniabramishvili94@gmail.com>
2023-02-08 17:32:41 +0530

    2 Commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,54 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import watchlist from './components/watchlist.vue';
+
+
+      const movies=[
+                    { 
+                        id: 1,
+                         name: "ყველაზე ჭკვიანი ცხოველები მსოფლიოში ★ ტოპ ფაქტები ★", 
+                         url:"https://www.youtube.com/watch?v=Mra2YR_wWXA", 
+                         description:"ვიდეოში გაგაცნობთ ყველაზე ჭკვიანი ცხოველებს მსოფლიოში,10. კატები,9. ვირთხები,8. სპილოები " ,
+                         cover:" http://kabisashvili.blogspot.com/p/cxovelebi.html "
+                    },
+                    { 
+                        id: 2, 
+                        name: "10 ამოუხსნელი ბუნებრივი ფენომენი  ", 
+                        url:"https://www.youtube.com/watch?v=x2wIhpy6sfA  ",
+                        description: "ვიდეოები იქნება მრავალ თემაზე, დედამიწა,გალაქტიკა,კოსმოსი,მეცნიერება,თეორიები,სამყარო,ცხოველთა სამყარო,ფრინველები,მცენარეები,ქვეწარმავლები,ანომალიური მოვლენები, ",
+                        cover:" https://yourshot.nationalgeographic.ge/saqartvelos-veluri-buneba/?contest=photo-detail&photo_id=1358  "
+                    },
+                    { 
+                        id: 3, 
+                        name: " სახიფათო ცხოველები უწყინარი გარეგნობით ★ ტოპ ფაქტები ★ ", 
+                        url:" https://www.youtube.com/watch?v=VA6WpcxNT-U ",
+                        description: "ვიდეოში ვისაუბრებთ სახიფათო და სასიკვდილო ცხოველების შესახებ, რომლებსაც საკმაოდ უწყინარი გარეგნობა აქვთ. ",
+                        cover:"  https://yourshot.nationalgeographic.ge/saqartvelos-veluri-buneba/?contest=photo-detail&photo_id=1354"
+                    },
+                    { 
+                        id: 4, 
+                        name: "10 ყველაზე ლამაზი ადგილი დედამიწაზე  ", 
+                        url:" https://www.youtube.com/watch?v=05P_dKyAcr8&t=6s ",
+                        description:" 10.ყინულის გამოქვაბული ვატნაიიოკიუდლ9.ბამბუკის ტყე საგანო8.ზღვის გამოქვაბული ალგარვე7.მთა რორაიმა6.იგუასუს ჩანჩქერები",
+                        cover:" https://yourshot.nationalgeographic.ge/saqartvelos-veluri-buneba/?contest=photo-detail&photo_id=1357 "
+
+                    },
+                    { 
+
+                        id: 5, 
+                        name: " 10 საქართველოს ყველაზე ლამაზი ადგილი ", 
+                        url:" https://www.youtube.com/watch?v=6EboEbD60xc ",
+                        description: "ვიდეოები იქნება მრავალ თემაზე 10 საქართველოს ყველაზე ლამაზი ადგილი  ",
+                        cover:" https://yourshot.nationalgeographic.ge/saqartvelos-veluri-buneba/?contest=photo-detail&photo_id=1359 "
+                    }
+                  ]
+  
+
+
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="container flex flex-row"> 
+   <watchlist :data="movies"/>
+  
+  </div>
 </template>
---
+++ src/components/watchlist.vue
@@ -0,0 +1,17 @@
+<script >
+export default{ 
+    props: {
+        data:{type: Array}
+     }
+    }
+ 
+</script>
+
+<template>
+ <div class="w-full" > 
+    <ul>
+        <li v-for=" name in data" :key="name.id">{{ name.id }} -{{ name.name }} -{{ name.url }}-{{ name.description }}-{{ name.cover }}</li>
+    </ul>
+ </div>
+
+</template>
